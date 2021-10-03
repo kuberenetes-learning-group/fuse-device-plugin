72a0af0b36aafdfeafe545f85a7108b53583536c
soolaugust <soolaugust@gmail.com>
2021-10-03 21:07:03 +0800

    add log

--- watchers.go
+++ watchers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"github.com/fsnotify/fsnotify"
 	"os"
 	"os/signal"
@@ -9,12 +10,14 @@ import (
 func newFSWatcher(files ...string) (*fsnotify.Watcher, error) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
+		log.Errorf("new watcher meet error:%s\n", err.Error())
 		return nil, err
 	}
 
 	for _, f := range files {
 		err = watcher.Add(f)
 		if err != nil {
+			log.Errorf("add watcher meet error:%s\n", err.Error())
 			watcher.Close()
 			return nil, err
 		}
